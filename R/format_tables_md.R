1ffb1f016814bf7d48aa3fd9cf91a732fcb0d5f9
BillPetti <billpetti@gmail.com>
2019-05-07 10:55:41 -0400

    First substantive commit - added format_tables function for markdown tables - edited DESCRIPTION

---
+++ R/format_tables_md.R
@@ -0,0 +1,28 @@
+#' Format Markdown Tables
+#'
+#' @param df A data frame to view as a table
+#' @param caption A caption, if needed
+#'
+#' @importFrom knitr kable
+#' @importFrom kableExtra kable_styling
+#' @return A formatted data frame to display in a markdown document
+#' @export
+#'
+#' @examples
+format_tables <- function(df, caption = NA) {
+  if (!is.na(caption)) {
+    df_table <- df %>%
+      knitr::kable(caption = caption) %>%
+      kableExtra::kable_styling(position = "c",
+                    bootstrap_options = "striped",
+                    full_width = FALSE)
+  } else {
+    df_table <- df %>%
+      knitr::  kable() %>%
+      kableExtra::kable_styling(position = "c",
+                    bootstrap_options = "striped",
+                    full_width = FALSE)
+  }
+
+  df_table
+}
